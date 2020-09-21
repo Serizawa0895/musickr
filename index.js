5c7a6b1a17f6c1ce5e0b1051d5bc6cdd71a53b01
Serizawa1711 <66017545+Serizawa0895@users.noreply.github.com>
2020-09-21 13:17:28 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = 'NzU3MjE1OTM5Njk0NTU5MzMz.X2dKTA.VIwVkX6MRfwMDqVtFwzOKCsHJXI';
+const token = process.env.token;
 const welcomeChannelName = "안녕하세요";
 const byeChannelName = "안녕히가세요";
 const welcomeChannelComment = "어서오세요.";
